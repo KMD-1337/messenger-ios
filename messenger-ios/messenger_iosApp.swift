31b410c40be687bca1c606ad9a75cb55534f0adf
andreichenko256 <andreichenko.kirill2000@gmail.com>
2020-12-02 17:13:51 +0200

    Initial Commit

---
+++ messenger-ios/messenger_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  messenger_iosApp.swift
+//  messenger-ios
+//
+//  Created by Kirill on 02.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct messenger_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
