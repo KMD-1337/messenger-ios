f4ac7fc9f332d6b0f47c50981fe1c2d85bfc11b1
andreichenko256 <andreichenko.kirill2000@gmail.com>
2020-12-21 19:25:26 +0200

    added log in and register screens

---
+++ messenger-ios/Resources/Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  Extensions.swift
+//  messenger-ios
+//
+//  Created by Kirill on 21.12.2020.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return self.frame.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return self.frame.width + self.frame.origin.x
+    }
+}
